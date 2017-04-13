db0cb111c43b6d19b8dfab13748ba31100dcbf30
1_am_a_geek <tmy0x3@icloud.com>
2017-04-13 20:37:42 +0900

    Initial Commit

---
+++ AssemblyLine/AssemblyLine.h
@@ -0,0 +1,19 @@
+//
+//  AssemblyLine.h
+//  AssemblyLine
+//
+//  Created by 1amageek on 2017/04/13.
+//  Copyright © 2017年 Stamp Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AssemblyLine.
+FOUNDATION_EXPORT double AssemblyLineVersionNumber;
+
+//! Project version string for AssemblyLine.
+FOUNDATION_EXPORT const unsigned char AssemblyLineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AssemblyLine/PublicHeader.h>
+
+
